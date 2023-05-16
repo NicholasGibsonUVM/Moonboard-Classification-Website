3052b1b78aae9c356fd26586a9bb4d066d2db837
NicholasGibsonUVM <nsgibson@uvm.edu>
2023-05-16 16:29:42 -0400

    fixed build issue

--- src/features/moonboard/boardSlice.js
+++ src/features/moonboard/boardSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   board: [...Array(19 * 12).keys()].map((i) => {
